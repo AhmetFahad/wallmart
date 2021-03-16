618f909d7afd5c8f7906f48d971593c89b07afc2
Ahmet Fahad <ahmadfahad6394@gmail.com>
2021-03-16 20:04:02 +0300

    Test

---
+++ seeder/seeder.rb
@@ -0,0 +1,6 @@
+pages << {
+    page_type: "listings",
+    method: "GET",
+    url: "https://www.walmart.com/browse/movies-tv-shows/4096?facet=new_releases:Last+90+Days",
+    fetch_type: "browser"
+  }
